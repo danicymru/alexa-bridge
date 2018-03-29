8ba97e697f3525709a26c54d336114e9044c016c
danicymru <37866461+danicymru@users.noreply.github.com>
2018-03-29 10:25:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var bridge = require("./lib/alexaBridge.js");
 
 function main() {
+  const appInsights = require('applicationinsights');
+  appInsights.setup('5d89c01a-ba2f-42e0-9db0-2dfc30c850e5').start();
   console.log('Starting...');
   bridge.start();
 }
